ea402b30b966485e584ff00fc404b978f2dc69f1
nuremberggroupjo-ship-it <nuremberggroupjo@gmail.com>
2025-12-31 19:02:44 +0300

    edit4

--- components/missionvissioncomponents/CTASection.tsx
+++ components/missionvissioncomponents/CTASection.tsx
@@ -1,11 +1,16 @@
+import Button1 from "../ui/Button1"
+import Link from "next/link"
+
 export default function CTASection({ isAr }: { isAr: boolean }) {
   return (
     <section className="bg-[#397a34] text-white py-24 text-center">
       <h2 className="text-3xl centert font-bold mb-4">{isAr ? "كن جزءًا من رحلتنا" : "Be Part of Our Journey"}</h2>
       <p className="mb-8 centert opacity-90">{isAr ? "ابدأ رحلتك التعليمية اليوم واصنع مستقبلك." : "Start your learning journey today and build your future."}</p>
-      <button className="bg-white text-[#397a34] px-8 py-3 rounded-full font-semibold hover:bg-green-100 transition">
+      <Link href={"/programs/form"}>
+     <Button1>
         {isAr ? "سجل الآن" : "Join Now"}
-      </button>
+      </Button1>
+      </Link>
     </section>
   )
 }
